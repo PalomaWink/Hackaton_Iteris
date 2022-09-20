9903b22beb6f4d71e0ee535f27a7be2854af8e54
Paloma Wink <108155575+PalomaWink@users.noreply.github.com>
2022-09-20 12:55:17 -0300

    JS

---
+++ Exercicios/script.js
@@ -0,0 +1,27 @@
+var a;
+let b;
+const c = 5;
+
+// Valores, tipos e operadores
+let numero  = 0;
+let boleano = true;
+let texto = '';
+let data  = new Date(); //data de hoje
+
+// Tipos
+typeof numero;
+typeof boleano;
+typeof texto;
+typeof data;
+
+// Operadores
+numero == '0'; //true
+numero === 0; //true
+!boleano; //false
+data > new Date(2022, 09, 20)
+// null e undefined
+let indefinido;
+let nulo = null;
+// Tipos
+typeof indefinido; //undefined
+typeof nulo; //objeto
