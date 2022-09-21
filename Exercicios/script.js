a30004fd3f3feed1c78f8da7686cb55dadbbdac6
Paloma Wink <108155575+PalomaWink@users.noreply.github.com>
2022-09-21 11:47:38 -0300

    API e Requisições HTTP

--- Exercicios/script.js
+++ Exercicios/script.js
@@ -1,27 +1,90 @@
-var a;
-let b;
-const c = 5;
+/**
+ * Envia dados para API
+ * @param {{ tipo: number; nome: string }} novaSubmissao
+ * @returns {Promise<{ tipo: number; nome: string }>} Nova submissão
+ */
+ const postData = (novaSubmissao) =>
+ fetch(`http://localhost:3000/submissoes`, {
+   method: "POST",
+   headers: {
+     "Content-Type": "application/json",
+   },
+   body: JSON.stringify(novaSubmissao),
+ })
+   .then((response) => response.json())
+   .catch((err) => console.error(err));
 
-// Valores, tipos e operadores
-let numero  = 0;
-let boleano = true;
-let texto = '';
-let data  = new Date(); //data de hoje
+/**
+* Consulta dados na API
+* @returns {Promise<{ tipo: number; nome: string }[]>} Lista de submissões
+*/
+const getData = () =>
+ fetch("http://localhost:3000/submissoes")
+   .then((response) => response.json())
+   .catch((err) => console.error(err));
 
-// Tipos
-typeof numero;
-typeof boleano;
-typeof texto;
-typeof data;
+/**
+ * Executa o submit do formulário da página
+ * @param {Event} event Evento de submit
+ */
+ const onSubmit = (event) => {
+    event.preventDefault();
+    const ulEl = document.querySelector(".main ul");
+  
+    if (ulEl) {
+      const submitedValue = {
+        nome: event.target.nome.value,
+        tipo: event.target.tipo.value,
+      };
+  
+      // Trabalhando com promises sem uso do async/await
+      postData(submitedValue).then((novaSubmissao) => {
+        addListItem(novaSubmissao, ulEl);
+      });
+    }
+  };
+  
+  /**
+   * Adiciona item na lista do html
+   * @param {{ tipo: number; nome: string }} item
+   * @param {HTMLUListElement} ulEl 
+   */
+  const addListItem = (item, ulEl) => {
+    const liEl = document.createElement("li");
+    liEl.innerText = `${item.nome} - Tipo: ${item.tipo}`;
+    ulEl.appendChild(liEl);
+  } 
+  
+/**
+ * Adiciona lista de submits consultada
+ * @param {HTMLUListElement} ulEl
+ */
+ const addSubmits = async (ulEl) => {
+    // Trabalhando com promises usando async/await
+    const result = await getData();
+  
+    // Se result existir e tiver elementos no array
+    if (result && result.length) {
+      result.forEach((item) => addListItem(item, ulEl));
+    }
+  };
 
-// Operadores
-numero == '0'; //true
-numero === 0; //true
-!boleano; //false
-data > new Date(2022, 09, 20)
-// null e undefined
-let indefinido;
-let nulo = null;
-// Tipos
-typeof indefinido; //undefined
-typeof nulo; //objeto
+/**
+ * executa após carregamento do conteúdo do DOM
+ */
+ const onLoad = () => {
+    const footerEl = document.querySelector(".footer p");
+    const formEl = document.querySelector("form");
+    const ulEl = document.querySelector(".submits ul");
+    // verifica o elemento buscado
+    if (footerEl)
+      footerEl.innerHTML += ` - ${new Date().getFullYear()}`;
+    // verifica o elemento buscado
+    if (formEl)
+      formEl.addEventListener('submit', onSubmit);
+    // verifica o elemento buscado
+    if (ulEl)
+      addSubmits(ulEl);
+    // remove evento do documento
+    document.removeEventListener('DOMContentLoaded', onLoad);
+  };
